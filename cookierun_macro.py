d302abaefd477735538452fa33225dfe8624f1a6
syki66 <66syki@gmail.com>
2018-11-15 13:48:48 +0900

    update cookierun_macro.py v1.0

--- cookierun_macro.py
+++ cookierun_macro.py
@@ -12,32 +12,34 @@ width, height = pyautogui.size()
 print('width={0}, height={1}'.format(width, height))
 
 #안전모드, 에러시 탈출구 (마우스 왼쪽 위 모서리에 가져가면 에러 송출)
-pyautogui.PAUSE = 1 #메쏘드간 대기시간
+pyautogui.PAUSE = 0.6 #메쏘드간 대기시간
 pyautogui.FAILSAFE = True
 
 #매크로
 
-
 start_t = time.time()
-
+time.sleep(2)
+pyautogui.click(1590, 893)
 while(True):
-    pyautogui.click(1400, 600)
-    pyautogui.click(1400, 600)
-    pyautogui.click(950,540)
+    pyautogui.doubleClick(1815, 469)
+    pyautogui.click(1815, 469)
+    pyautogui.click(1080, 531)
     end_t = time.time()
     
-    if (end_t - start_t) > 300:
+    if (end_t - start_t) > 340:
         start_t = end_t
-        time.sleep(10)
-        pyautogui.click(689, 936) #게임점수확인창
         time.sleep(5)
-        pyautogui.click(1083, 937) #보물상자 열기
-        time.sleep(5)
-        pyautogui.click(1083, 937) #보물상자 확인
-        time.sleep(5)
-        pyautogui.click(1522,937) #게임시작1
-        time.sleep(5)
-        pyautogui.click(1522,937) #게임시작 2
+        pyautogui.click(689, 936) #게임점수확인창(자랑하기 y축 870~1008, 보물상자 동일)
+        time.sleep(3)
+        pyautogui.click(884, 970) #보물상자 열기
+        time.sleep(3)
+        pyautogui.click(884, 970) #보물상자 확인
+        time.sleep(2)
+        pyautogui.click(1573, 277) #보물상자 없을시 포춘쿠키창 끄기
+        time.sleep(2)
+        pyautogui.click(1590, 893) #게임시작1
+        time.sleep(2)
+        pyautogui.click(1590, 893) #게임시작 2
         
 
 
@@ -46,3 +48,6 @@ while(True):
 #img1.show()
 
 #print(pytesseract.image_to_string(img1))
+
+
+#print(pyautogui.position())
