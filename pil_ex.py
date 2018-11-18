55b862cceda38f8267a7c68f1431fd073728bfc9
syki66 <66syki@gmail.com>
2018-11-18 18:57:50 +0900

    update pil_ex.py

--- pil_ex.py
+++ pil_ex.py
@@ -1,54 +1,62 @@
+
 from PIL import ImageGrab
 from PIL import Image
 import matplotlib.pyplot as plt
 
-
-#img1 = ImageGrab.grab(bbox=(0, 0, 300, 300)) #(x1,y1)부터 (x2,y2) 까지 캡처
-#img1.save('abc.png')
-#img2 = Image.open('abc.png')
-#coordinate = (0,0)
-#print(img2.getpixel(coordinate))
-
-img1 = Image.open('pr3.png')
+img1 = Image.open('st5.png')
 img1_black = img1.convert("L")
-#img1.save('pr3.png')
+#img1_black.show()
 
 
 result1 = []
 result2 = []
 result3 = []
-for i in range(30,215):
-    c = (i, 200)
+for i in range(50,150):
+    c = (i,180)
     if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
         print("", img1_black.getpixel(c), end='')
         result1.append(img1_black.getpixel(c))
-        
-    if i % 10 == 0:
+
+    if i%10 == 0 :
         print("")
 
-for i in range(326, 455):
-    c = (i, 200)
+print("\n========================================")
+
+for i in range(300,400):
+    c = (i,180)
     if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
         print("", img1_black.getpixel(c), end='')
         result2.append(img1_black.getpixel(c))
-        
-    if i % 10 == 0:
+
+    if i%10 == 0 :
         print("")
-    
-for i in range(606, 797):
-    c = (i, 200)
+
+print("\n========================================")
+
+for i in range(550,650):
+    c = (i,180)
     if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
         print("", img1_black.getpixel(c), end='')
         result3.append(img1_black.getpixel(c))
-        
-    if i % 10 == 0:
+
+    if i%10 == 0 :
         print("")
+print("\n========================================")
+
+result2.insert(0,188)
+
+for i in range(3,48):
+    print(abs(result3[i]-result1[i]))
+
+
+
 
 plt.plot(result1)
 plt.plot(result2)
 plt.plot(result3)
-
-for i in range(100):
-    print(abs(result2[i]-result1[i]))
-
 plt.show()
+
+
+
+
+    #if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
