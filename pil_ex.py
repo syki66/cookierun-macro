7259f50a44bb63df72a832157fd31210106c750d
syki66 <66syki@gmail.com>
2018-11-18 18:57:31 +0900

    update pil_ex.py

--- pil_ex.py
+++ pil_ex.py
@@ -1,6 +1,7 @@
-import pyautogui
 from PIL import ImageGrab
 from PIL import Image
+import matplotlib.pyplot as plt
+
 
 #img1 = ImageGrab.grab(bbox=(0, 0, 300, 300)) #(x1,y1)부터 (x2,y2) 까지 캡처
 #img1.save('abc.png')
@@ -8,31 +9,46 @@ from PIL import Image
 #coordinate = (0,0)
 #print(img2.getpixel(coordinate))
 
-img1 = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
-img1.show()
+img1 = Image.open('pr3.png')
+img1_black = img1.convert("L")
+#img1.save('pr3.png')
 
-c1_result = []
-for i in range(349,528):
-    c = (i,218)
-    c1_result.append(list(img1.getpixel(c)))
-    #print(",",i, "--", img1.getpixel(c), end='')
+
+result1 = []
+result2 = []
+result3 = []
+for i in range(30,215):
+    c = (i, 200)
+    if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
+        print("", img1_black.getpixel(c), end='')
+        result1.append(img1_black.getpixel(c))
+        
+    if i % 10 == 0:
+        print("")
+
+for i in range(326, 455):
+    c = (i, 200)
+    if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
+        print("", img1_black.getpixel(c), end='')
+        result2.append(img1_black.getpixel(c))
+        
+    if i % 10 == 0:
+        print("")
     
-    #if i % 10 == 0:
-    #    print("")
+for i in range(606, 797):
+    c = (i, 200)
+    if img1_black.getpixel(c) != 239 and img1_black.getpixel(c) != 231:
+        print("", img1_black.getpixel(c), end='')
+        result3.append(img1_black.getpixel(c))
+        
+    if i % 10 == 0:
+        print("")
 
-c2_result = []
-for i in range(545,724):
-    c1 = (i, 218)
-    c2_result.append(list(img1.getpixel(c)))
-print(c1_result)
+plt.plot(result1)
+plt.plot(result2)
+plt.plot(result3)
 
+for i in range(100):
+    print(abs(result2[i]-result1[i]))
 
-'''
-img2 = ImageGrab.grab(bbox=(524, 293, 1071, 294))
-img2.show()
-'''
-#print(pyautogui.position())
-
-#349~527
-
-#545~723
+plt.show()
