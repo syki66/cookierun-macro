4ba80ffb28566d530008beb4ea948793045b3dfe
syki66 <66syki@gmail.com>
2018-11-19 00:45:30 +0900

    update pil_ex.py

--- pil_ex.py
+++ pil_ex.py
@@ -2,48 +2,97 @@ from PIL import ImageGrab
 from PIL import Image
 import matplotlib.pyplot as plt
 
-
-img1 = ImageGrab.grab(bbox=(0,0,1920,1080))
-img1_black = img1.convert("L")
-img1_black.show()
-img1_black.save('fi2.png')
-
-#(548,526) , (548,887)    x좌표 768까지
-
-result1 = []
-result2 = []
-#+ 220픽셀 비교
-#첫번째 이미지 (548,526)  ,,기준으로 나머지 5개 비교하기
-#두번째 이미지 (823,526)
-#세번째 이미지 (1098,526)
-#네번째 이미지 (548,887)
-#오번째 이미지 (823,887)
-#육번째 이미지 (1098,887)
-#
-for i in range(548,768):
-    c = (i, 526)
-    print("", img1_black.getpixel(c), end='')
-    result1.append(img1_black.getpixel(c))
-    if i % 10 == 0:
-        print("")
-
-for i in range(548,768):
-    c = (i, 887)
-    print("", img1_black.getpixel(c), end='')
-    result2.append(img1_black.getpixel(c))
-    if i % 10 == 0:
-        print("")
+def Image_Classify():
+    img1 = ImageGrab.grab(bbox=(0,0,1920,1080))
+    img1_black = img1.convert("L")
+    #img1_black.show()
+    img1_black.save('fi2.png')
     
+    #(548,526)얘가 기준값 , (548,886)    x좌표 768까지
     
+    result1 = []
+    result2 = []
+    result3 = []
+    result4 = []
+    result5 = []
+    result6 = []
+        
+    for i in range(548,768):
+        c = (i, 526)
+        result1.append(img1_black.getpixel(c))
+    
+    for i in range(548,768):
+        c = (i, 887)
+        result4.append(img1_black.getpixel(c))
+        
+    for i in range(823,1043):
+        c = (i, 526)
+        result2.append(img1_black.getpixel(c))
+    
+    for i in range(823,1043):
+        c = (i, 887)
+        result5.append(img1_black.getpixel(c))
+        
+    for i in range(1098,1318):
+        c = (i, 526)
+        result3.append(img1_black.getpixel(c))
+        
+    for i in range(1098,1318):
+        c = (i, 887)
+        result6.append(img1_black.getpixel(c))
+    
+    sum = [0,0,0,0,0]    
+    
+    for i in range(len(result1)):
+        sum[0] += abs(result2[i]-result1[i])
+        
+    for i in range(len(result1)):
+        sum[1] += abs(result3[i]-result1[i])
+        
+    for i in range(len(result1)):
+        sum[2] += abs(result4[i]-result1[i])
+    
+    for i in range(len(result1)):
+        sum[3] += abs(result5[i]-result1[i])
+        
+    for i in range(len(result1)):
+        sum[4] += abs(result6[i]-result1[i])
+    
+    result = []
+    for i in range(len(sum)):
+        if sum[i] < 1000:
+            print(sum[i])
+            result.append(i)
+    print(result)
+    if len(result) == 3:
+        
+        
+        
+        
+        #1번째 그림클릭과
+        print(result[0])
+        print(result[1])# 클릭으로 변경
+        print(result[2])
+    else:
+        print(result[0])
+        #그리고 1번째그림 클릭
+        
+        
+        
+        
+Image_Classify()
+
+
+
+
+
+'''
 plt.plot(result1)
 plt.plot(result2)
-
-#합계 기준 1000으로 1000보다 같으면 동일이미
-
-'''
-sum = 0
-for i in range(len(result1)):
-    sum += abs(result2[i]-result1[i])
+plt.plot(result3)
+plt.plot(result4)
+plt.plot(result5)
+plt.plot(result6)
+plt.show()
+''' 
     
-print("합계는", sum)
-'''
