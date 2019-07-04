2b87f85f26770fcbc45a71498a5e863a1a25487d
syki66 <66syki@gmail.com>
2019-07-04 22:36:49 +0900

    update cookierun_macro.py v3.0

--- cookierun_macro.py
+++ cookierun_macro.py
@@ -2,26 +2,26 @@ import pyautogui
 import sys
 import time
 import image
-import pytesseract
+#import pytesseract
 from PIL import ImageGrab
 from PIL import Image
 
 width, height = pyautogui.size()  
 print("================================================================")
-print("쿠키런매크로 v2.0")
+print("쿠키런매크로 v3.0 --- 2019.07.04")
 print("현재해상도:", width, height)
-print("해상도 1920x1080에서 최대창모드에서만 정상동작")
-print("추가사항: 쿠키런 매크로 방지 뚫기 기능추가 및 하트대기시간 조정")
+print("해상도 1920x1080에서 전체화면에서만 정상동작")
+print("추가사항: 별플레이어 섭종에 따른 풀스크린 대응(좌표값변경), 드래그 이용한 슬라이딩 추가, 기록초기화시 확인버튼 자동")
 print("================================================================")
 
 #안전모드, 에러시 탈출구 (마우스 왼쪽 위 모서리에 가져가면 에러 송출)
-pyautogui.PAUSE = 0.5 #메쏘드간 대기시간
+pyautogui.PAUSE = 0.3 #메쏘드간 대기시간
 pyautogui.FAILSAFE = True
+
 
 #쿠키런 매크로 방지 뚫는 함수
 
-img_position = [(558, 315),(867, 317),(1102, 595),(592, 773),(926, 952),(1106, 678)]
-
+img_position = [(593, 362),(948, 360),(1123, 404),(586, 959),(883, 986),(1156, 705)]
 
 def Image_Classify():
     img1 = ImageGrab.grab(bbox=(0,0,1920,1080))
@@ -29,7 +29,7 @@ def Image_Classify():
     #img1_black.show()
     #img1_black.save('fi2.png')
     
-    #(548,526)얘가 기준값 , (548,886)    x좌표 768까지
+    #(534,500)얘가 기준값
     result1 = []
     result2 = []
     result3 = []
@@ -37,28 +37,31 @@ def Image_Classify():
     result5 = []
     result6 = []
     
-    for i in range(548,768):
-        c = (i, 526)
+
+    img_y1 = 500
+    img_y2 = 885
+    for i in range(534,754):
+        c = (i, img_y1)
         result1.append(img1_black.getpixel(c))
-    
-    for i in range(548,768):
-        c = (i, 887)
+
+    for i in range(534,754):
+        c = (i, img_y2)
         result4.append(img1_black.getpixel(c))
-        
-    for i in range(823,1043):
-        c = (i, 526)
+            
+    for i in range(828,1048):
+        c = (i, img_y1)
         result2.append(img1_black.getpixel(c))
-    
-    for i in range(823,1043):
-        c = (i, 887)
+        
+    for i in range(828,1048):
+        c = (i, img_y2)
         result5.append(img1_black.getpixel(c))
-        
-    for i in range(1098,1318):
-        c = (i, 526)
+            
+    for i in range(1121,1341):
+        c = (i, img_y1)
         result3.append(img1_black.getpixel(c))
-        
-    for i in range(1098,1318):
-        c = (i, 887)
+            
+    for i in range(1121,1341):
+        c = (i, img_y2)
         result6.append(img1_black.getpixel(c))
     
     sum = [0,0,0,0,0]    
@@ -83,7 +86,7 @@ def Image_Classify():
     
     result = []
     for i in range(len(sum)):
-        if sum[i] > 1000:
+        if sum[i] > 1200:
             print(sum[i])
             result.append(i+1) #두번째그림이 sum[0]이라서 1추가함
     print("차출된값: ", result)
@@ -126,39 +129,46 @@ def Image_Classify():
 
 
 
-
 #인게임 매크로
 game_count = 0
 start_t = time.time()
 time.sleep(2)
-pyautogui.click(1590, 893)
+pyautogui.click(1620, 914) #게임시작2
+
 while(True):
     
-    pyautogui.doubleClick(1815, 469)
-    pyautogui.click(1815, 469)
-    pyautogui.click(1080, 531)
+    pyautogui.click(1620, 914) #점프
+    pyautogui.click(958,400) # 슬라이드 및 부스트 및 이어달리기
+    pyautogui.dragTo(958,380, 0.5, button='left') # 슬라이드 용 드래그
+    pyautogui.click(1620, 914) #점프
+    
     end_t = time.time()
     
     if (end_t - start_t) > 460:
         game_count += 1 #게임 판수 세기
-        print("**************", game_count,"판")
+        print("******************************", game_count,"판")
         start_t = end_t
         time.sleep(5)
-        for i in range(6):
-            time.sleep(2)
+        for i in range(4):
+            time.sleep(1)
             Image_Classify() #매크로방지 뚫기함수
-            time.sleep(2)
-        time.sleep(5)
-        pyautogui.click(689, 936) #게임점수확인창(자랑하기 y축 870~1008, 보물상자 동일)
+            time.sleep(1)
+
+        #test_img = ImageGrab.grab(bbox=(0,0,1920,1080)) # 에러난거 테스트
+        #test_img.save(str(game_count)+".png") # 에러난거 테스트
+        
         time.sleep(3)
-        pyautogui.click(884, 970) #보물상자 열기
+        pyautogui.click(883, 986) #게임점수확인창(자랑하기 x축 984~1458, y축 887 아래로 보물상자 동일)
         time.sleep(3)
-        pyautogui.click(884, 970) #보물상자 확인
+        pyautogui.click(883, 986) #보물상자 열기
+        time.sleep(3)
+        pyautogui.click(883, 986) #보물상자 확인
         time.sleep(2)
-        pyautogui.click(1573, 277) #보물상자 없을시 포춘쿠키창 끄기
+        pyautogui.click(1632, 259) #보물상자 없을시 포춘쿠키창 끄기
         time.sleep(2)
-        pyautogui.click(1590, 893) #게임시작1
+        pyautogui.click(1733, 963) #게임시작1
         time.sleep(2)
-        pyautogui.click(1590, 893) #게임시작 2
+        pyautogui.click(1600, 914) #게임시작 2
+        time.sleep(1)
 
     
---
+++ reward_macro.py
@@ -0,0 +1,8 @@
+import pyautogui
+
+
+pyautogui.PAUSE = 0.1 #메쏘드간 대기시간
+pyautogui.FAILSAFE = True
+
+while(True):
+    pyautogui.click(1112,788)
