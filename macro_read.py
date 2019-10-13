f716ba638f32d6149afdd65ddc1efc31a071e5a8
syki66 <66syki@gmail.com>
2019-10-13 22:59:18 +0900

    update macro_read.py macro_write.py

--- macro_read.py
+++ macro_read.py
@@ -3,7 +3,6 @@ import pyautogui
 import time
 import csv
 
-pyautogui.PAUSE = 0
 f = open('macro_record.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
 
@@ -15,22 +14,21 @@ for line in rdr:
     tim.append(line[1])
 
 
-
+print(pos)
 time_float = list(map(float, tim))
 
-
+print(time_float)
 
 f.close()
 
+start_t = time.time()
 
 for i in range(len(pos)):
-    start_t = time.time()
     position_0 = (363,768)
     position_1 = (1455, 906)
 
     position = position_0 if pos[i] == "0" else position_1
     
-    time.sleep(time_float[i+1])
-
-    pyautogui.click(position)
-
+    print(position)
+    pyautogui.click(position, interval = time_float[i+1])
+    print (time.time()-start_t)
--- macro_write.py
+++ macro_write.py
@@ -3,7 +3,6 @@ import pyautogui
 import time
 import csv
 
-pyautogui.PAUSE = 0
 csvfile = open("macro_record.csv", "w", newline="")
 csvwriter = csv.writer(csvfile)
 
@@ -14,13 +13,13 @@ start_time = time.time()
 
 def is_clicked(x, y, button, pressed):
     if pressed:
-        x_pos = 0 if pyautogui.position()[0]<1280 else 1
+        x_pos = 0 if pyautogui.position()[0]<960 else 1
         running_time = (time.time()-start_time)
         each_data = [x_pos, running_time]
         full_data.append(each_data)
+        print(each_data)
 
-
-        if (time.time() - start_time > 60):
+        if (time.time() - start_time > 20):
             
             for row in full_data:
                 csvwriter.writerow(row)
@@ -30,5 +29,6 @@ def is_clicked(x, y, button, pressed):
 
 with Listener(on_click=is_clicked) as listener:
     listener.join()
+
 
 
